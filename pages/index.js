95fc55810fcf1c7f3436f7acc862500f224e5fa0
LEEHYUNBOK <34876989+LEEHYUNBOK@users.noreply.github.com>
2021-07-29 14:19:17 +0900

    init change

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,42 @@
 import useSwr from 'swr'
 import Link from 'next/link'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Index() {
+  
+  const [name,setName] = useState('')
+  const [value,setValue] = useState('')
+  
   const { data, error } = useSwr('/api/users', fetcher)
   console.log('index data', data)
-  console.log('index error', error)
   if (error) return <div>Failed to load users</div>
   if (!data) return <div>Loading...</div>
 
-  // const [name,setName] = useState('')
+  
 
   return (
     <div>
       <div>
-        {/* <input type="text" 
-        value={name}
-        onChange =
-        /> */}
-        <button>extes</button>
-        <h1></h1>
+        <input type="text"
+          name="name"
+          placeholder="이메일을 입력"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={(text) => setValue(name)}>extes</button>
+        <h1>{value}</h1>
       </div>
-    <ul>
-      {data.map((user) => (
-        <li key={user.id}>
-          <Link href="/user/[id]" as={`/user/${user.id}`}>
-            <a>{`${user.name} ${user.id}`}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            <Link href="/user/[id]" as={`/user/${user.id}`}>
+              <a>{`${user.name} ${user.id}`}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
