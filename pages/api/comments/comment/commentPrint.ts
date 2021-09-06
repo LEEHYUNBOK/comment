2a583ce1dd236ab7b7b4daa39aeb0027ae919801
LEEHYUNBOK <34876989+LEEHYUNBOK@users.noreply.github.com>
2021-09-06 14:21:26 +0900

    123523152531

--- pages/api/comments/comment/commentPrint.ts
+++ pages/api/comments/comment/commentPrint.ts
@@ -1,13 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../lib/prisma'
 
-export default async function CommentPrint(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function CommentPrint(req: NextApiRequest, res: NextApiResponse) {
   const { postId } = req.query
   const users = await prisma.comments.findMany({
-    // where: { postId: String(postId) },
+    where: { postId: String(postId) },
     include: { commentUsers: true },
   })
 
