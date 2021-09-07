8bad20c622fd598cf2b8d353b37911217f57d65f
LEEHYUNBOK <34876989+LEEHYUNBOK@users.noreply.github.com>
2021-09-07 11:11:57 +0900

    Merge branch 'feature/add-json'
    * feature/add-json:
      comment 조금 수정
      test
      1235253
      123523152531

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '../styles/suneditor.css'
-import type { AppProps } from 'next/app'
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default App
